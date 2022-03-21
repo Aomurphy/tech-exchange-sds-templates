4f1612cabd6021565923e34f3800694ca94b85ec
victorvegaward <victorandresvega@gmail.com>
2022-03-20 23:02:01 -0400

    started quiz frontend

--- flask-template/app.py
+++ flask-template/app.py
@@ -1,30 +1,93 @@
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-# ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
+state_names = ["Alaska", "Alabama", "Arkansas", "Arizona", "California", "Colorado", "Connecticut", "Delaware",
+ "Florida", "Georgia", "Hawaii", "Iowa", "Idaho", "Indiana", "Kansas", "Kentucky", "Louisiana", 
+ "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", 
+ "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "New York", "Ohio", "Oklahoma", 
+ "Oregon", "Pennsylvania", "South Carolina", "Tennessee", "Texas",
+  "Utah", "Virginia", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+capital_dic={
+    'Alabama': 'Montgomery',
+    'Alaska': 'Juneau',
+    'Arizona':'Phoenix',
+    'Arkansas':'Little Rock',
+    'California': 'Sacramento',
+    'Colorado':'Denver',
+    'Connecticut':'Hartford',
+    'Delaware':'Dover',
+    'Florida': 'Tallahassee',
+    'Georgia': 'Atlanta',
+    'Hawaii': 'Honolulu',
+    'Idaho': 'Boise',
+    'Illinios': 'Springfield',
+    'Indiana': 'Indianapolis',
+    'Iowa': 'Des Monies',
+    'Kansas': 'Topeka',
+    'Kentucky': 'Frankfort',
+    'Louisiana': 'Baton Rouge',
+    'Maine': 'Augusta',
+    'Maryland': 'Annapolis',
+    'Massachusetts': 'Boston',
+    'Michigan': 'Lansing',
+    'Minnesota': 'St. Paul',
+    'Mississippi': 'Jackson',
+    'Missouri': 'Jefferson City',
+    'Montana': 'Helena',
+    'Nebraska': 'Lincoln',
+    'Neveda': 'Carson City',
+    'New Hampshire': 'Concord',
+    'New Jersey': 'Trenton',
+    'New Mexico': 'Santa Fe',
+    'New York': 'Albany',
+    'North Carolina': 'Raleigh',
+    'North Dakota': 'Bismarck',
+    'Ohio': 'Columbus',
+    'Oklahoma': 'Oklahoma City',
+    'Oregon': 'Salem',
+    'Pennsylvania': 'Harrisburg',
+    'Rhoda Island': 'Providence',
+    'South Carolina': 'Columbia',
+    'South Dakoda': 'Pierre',
+    'Tennessee': 'Nashville',
+    'Texas': 'Austin',
+    'Utah': 'Salt Lake City',
+    'Vermont': 'Montpelier',
+    'Virginia': 'Richmond',
+    'Washington': 'Olympia',
+    'West Virginia': 'Charleston',
+    'Wisconsin': 'Madison',
+    'Wyoming': 'Cheyenne'  
+} 
+app = Flask('app')
+
+@app.route('/scorepage',methods=["POST","GET"])
+def output():
+  correctA=0
+  previous_correct=[]
+  for state in state_names:
+    if request.method=="POST":
+        answer = request.form['capital']
+           
+        correct = answer == capital_dic[state]
+        if(correct):
+            previous_correct.append(answer)  
+            correctA+=1
+  return render_template("scorepage.html", previous_correct=previous_correct,correct=correctA)
+  
+@app.route('/')
+def hello_world():
+  
+  return render_template("index.html",state_names=state_names)
 
 
-# -- Initialization section --
-app = Flask(__name__)
+app.run(host='0.0.0.0', port=8080)
 
 
 # -- Routes section --
-@app.route('/')
-@app.route('/index')
-def index():
-    return "hello world"
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     return "hello world"
